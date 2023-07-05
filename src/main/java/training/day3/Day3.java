3f9a2fa655ca8a76114bd795b3f54b67aa55b2c5
ProgrammerMT <murcho123@gmail.com>
2023-07-05 12:03:46 +0300

    Задачи-05.07.2023

---
+++ src/main/java/training/day3/Day3.java
@@ -0,0 +1,102 @@
+package training.day3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day3 {
+
+    public static void zadacha1(){
+        Scanner scann = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++){
+            System.out.println("Enter number: ");
+            int num = scann.nextInt();
+            arr[i] = num;
+        }
+
+        System.out.println("Positive numbers: ");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 0){
+                System.out.println(arr[i]);
+            }
+        }
+
+        System.out.println("Negative numbers: ");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] < 0){
+                System.out.println(arr[i]);
+            }
+        }
+
+        System.out.println("Odd numbers: ");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 != 0){
+                System.out.println(arr[i]);
+            }
+        }
+
+        System.out.println("Even numbers: ");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 == 0){
+                System.out.println(arr[i]);
+            }
+        }
+
+        System.out.println("Number of 5: ");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 5){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public static void zadacha2(String str1, String str2){
+        System.out.println(str1.replace(str2, ""));
+    }
+
+    public static void zadacha3(String str){
+        String[] arr = str.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void zadacha4(String name){
+        String[] words = name.split(" ");
+        for (int i = 0; i < words.length; i++){
+            System.out.println(words[i].charAt(0));
+        }
+
+    }
+
+    public static void zadacha5(ArrayList<String> list){
+        String first = list.get(0);
+        String last = list.get(list.size() - 1);
+        list.set(0, last);
+        list.set(list.size() - 1, first);
+
+        for (String words : list){
+            System.out.println(words);
+        }
+    }
+    public static void main(String[] args) {
+        //zadacha1();
+
+        String str1 = "Plovdiv";
+        String str2 = "lo";
+       // zadacha2(str1, str2);
+
+        String name = "Ivan Ivanov Ivanov";
+       // zadacha3(name);
+
+        //zadacha4(name);
+
+        ArrayList<String> list = new ArrayList();
+        list.add("Ivan");
+        list.add("GEorgi");
+        list.add("New");
+        list.add("Last");
+        list.add("More");
+        zadacha5(list);
+    }
+}
